ee56e5700af8b0e54b322879297d7fdf59939f58
Pangu <Pangu@DESKTOP-IF6DCCV>
2018-09-29 22:32:46 -0700

    added card base

---
+++ src/components/card/index.js
@@ -0,0 +1,15 @@
+import React,{ Component } from 'react';
+import '../../assets/css/card.css';
+
+class Card extends Component{
+
+
+    render(){
+        return (
+            <div className='card'></div>
+        );
+    }
+}
+
+
+export default Card;
