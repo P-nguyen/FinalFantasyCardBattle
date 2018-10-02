14a8d6e409a5aaf9fe6f293219cd441cc37c2e2d
Pangu <Pangu@DESKTOP-IF6DCCV>
2018-10-01 22:49:10 -0700

    added gameboard configured layout of all cards and added flipping functionalitiy cards have click handlers that add revealing class

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -2,11 +2,33 @@ import React,{ Component } from 'react';
 import '../../assets/css/card.css';
 
 class Card extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            revealed : false
+        }
+    }
 
+    revealCard(){
+        console.log('hellow');
+
+        this.setState({
+            revealed : true
+        });
+
+    }
 
     render(){
+        console.log(this.state);
+
         return (
-            <div className='card'></div>
+        <div className="cardContainer" onClick={this.revealCard.bind(this)}>
+            <div className={this.state.revealed ? "card flipper revealed" : "card flipper"}>
+                {/* <img className="back">yay</img> */}
+                <div className="back">yay</div>
+                <div className="front">yay</div>
+            </div>
+        </div>
         );
     }
 }
